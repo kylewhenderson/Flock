af838e1ef6c659f3711084bfbf3a760f62da2e7b
Kyle Henderson <kyle@jellyflea.com>
2016-09-16 21:16:03 -0500

    Initial Commit

---
+++ www/js/routes.js
@@ -0,0 +1,95 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.flockDashboard', {
+    url: '/dashboard',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/flockDashboard.html',
+        controller: 'flockDashboardCtrl'
+      }
+    }
+  })
+
+  .state('menu.skydives', {
+    url: '/skydives',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/skydives.html',
+        controller: 'skydivesCtrl'
+      }
+    }
+  })
+
+  .state('menu.addASkydive', {
+    url: '/add-skydive',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/addASkydive.html',
+        controller: 'addASkydiveCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.login', {
+    url: '/login',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('menu.myGear', {
+    url: '/gear',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/myGear.html',
+        controller: 'myGearCtrl'
+      }
+    }
+  })
+
+  .state('menu.viewSkydive', {
+    url: '/skydive',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/viewSkydive.html',
+        controller: 'viewSkydiveCtrl'
+      }
+    }
+  })
+
+  .state('login2', {
+    url: '/page8',
+    templateUrl: 'templates/login2.html',
+    controller: 'login2Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu/dashboard')
+
+  
+
+});
