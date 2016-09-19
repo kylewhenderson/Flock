0794e8e187006e038f5b7a381260a68873b93bb9
Kyle Henderson <kylehenderson@Kyles-MacBook.local>
2016-09-19 07:32:00 -0500

    Reverting SkydiveService

--- www/js/services.js
+++ www/js/services.js
@@ -9,7 +9,59 @@ angular.module('app.services', [])
 
 }])
 
+.service('SkydiveService', ['$http','$q',function($http,$q){
+    return {
+        getSkydives:function() {
+        
+            var deferred = $q.defer();
+            var rand = Math.floor(Math.random()*100); // probably not the right way to do this, but can't figure out another method.
+            var url = "http://logbook.jellyflea.net/wp-json/wp/v2/skydive?author=1&filter[posts_per_page]=10&rand=" + rand;
+            $http({ cache: false, url: url, method: 'GET'}).then(function(res) {
+                //console.dir(res);
+                deferred.resolve(res.data);
+            });
+            return deferred.promise;
+        },
+        getRecentSkydives:function() {
+            var deferred = $q.defer();
+            var url = "http://logbook.jellyflea.net/wp-json/wp/v2/skydive?author=1&filter[posts_per_page]=5";
+            $http({ cache: false, url: url, method: 'GET'}).then(function(res) {
+                deferred.resolve(res.data);
+            });
+            
+            return deferred.promise;
+        },
+        getSkydive:function(id) {
+            var deferred = $q.defer();
+      
+            $http.get("http://logbook.jellyflea.net/wp-json/wp/v2/skydive/"+id).then(function(res) {
+                //console.dir(res.data);
+                var skydive = {
+                    id:id,
+                    title:"Skydive #"+res.data.acf.jump_number,
+                    jump_number:res.data.acf.jump_number,
+                    jump_date:res.data.acf.jump_date,
+                    jump_location:res.data.acf.jump_location.post_title,
+                    jump_aircraft:res.data.acf.jump_aircraft.post_title,
+                    jump_type:res.data.acf.jump_type,
+                    jump_altitude:res.data.acf.jump_altitude,
+                    jump_gear_mode:res.data.acf.jump_gear_mode.label,
+                    jump_rig:res.data.acf.jump_rig.label,
+                    jump_container:res.data.acf.jump_container,
+                    //jump_main:res.data.acf.jump_main,
+                    //jump_reserve:res.data.acf.jump_reserve.label,
+                    //jump_cut_away:res.data.acf.jump_cut_away.label,
+                    // videos probably need to have their own service passing the skydive id
+                };
+                deferred.resolve(skydive);
+            });
+            return deferred.promise;      
+        }   
+    };
 
+}]);
+
+/*
 
 .service('SkydiveService', ['$http',function($http){
 
@@ -27,3 +79,4 @@ angular.module('app.services', [])
     }
   };
 }]);
+*/
