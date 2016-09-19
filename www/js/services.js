c61fc209c5467977b808f590760fd5211e095c6e
Kyle Henderson <kylehenderson@Kyles-MacBook.local>
2016-09-19 07:05:43 -0500

    Trying to find Phonegap error

--- www/js/services.js
+++ www/js/services.js
@@ -11,13 +11,18 @@ angular.module('app.services', [])
 
 
 
-.factory('SkydiveService', function($http) {
+.service('SkydiveService', function($http) {
 
   return {
     getSkydives:function() {
         // Return promise (async callback)
         var rand = Math.floor(Math.random()*100); // probably not the right way to do this, but can't figure out another method.
         var url = "http://logbook.jellyflea.net/wp-json/wp/v2/skydive?author=1&filter[posts_per_page]=10&rand=" + rand;
+        return $http({ cache: false, url: url, method: 'GET'});
+    },
+      
+    getSkydive:function(id) {
+        var url = "http://logbook.jellyflea.net/wp-json/wp/v2/skydive/" + id;
         return $http({ cache: false, url: url, method: 'GET'});
     }
   };
