430cb4ff2b5942032c5478abbfe112fd2d98e441
Kyle Henderson <kylehenderson@Kyles-MacBook.local>
2016-09-24 14:22:13 -0500

    Enabled Comments in Controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,22 +22,21 @@ function ($scope, $stateParams, SkydiveService) {
      
 }])
 
-.controller('skydiveCtrl', ['$scope', '$stateParams', 'SkydiveService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('skydiveCtrl', ['$scope', '$stateParams', 'SkydiveService', 'CommentService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, SkydiveService) {
+function ($scope, $stateParams, SkydiveService, CommentService) {
+   
     $scope.skydive = {};
-    //$scope.comments = {};
-    
     SkydiveService.getSkydive($stateParams.id).then(function(res) {
         $scope.skydive = res.data;  
     });
     
-    /*
+    $scope.comments = {};
     CommentService.getComments($stateParams.id).then(function(res) {
         $scope.comments = res.data;  
     });
-    */
+    
     $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
       viewData.enableBack = true;
     }); 
